c22a90e525abaee8ad73cf2534622a67e9795889
Òscar del Álamo i Guaus <alamoscar24@gmail.com>
2022-12-11 18:33:32 +0100

    Pass black

--- day6/script_1.py
+++ day6/script_1.py
@@ -5,8 +5,7 @@ if __name__ == "__main__":
     _input = lines.split("\n")
     data = _input[0]
     for index in range(1, len(data), 1):
-        last_four_chars = data[index-1:index+3]
+        last_four_chars = data[index - 1 : index + 3]
         if len(set(x for x in last_four_chars)) == 4:
-            print(index+3)
+            print(index + 3)
             break
-        
--- day6/script_2.py
+++ day6/script_2.py
@@ -5,8 +5,7 @@ if __name__ == "__main__":
     _input = lines.split("\n")
     data = _input[0]
     for index in range(1, len(data), 1):
-        last_four_chars = data[index-1:index+13]
+        last_four_chars = data[index - 1 : index + 13]
         if len(set(x for x in last_four_chars)) == 14:
-            print(index+13)
+            print(index + 13)
             break
-        
