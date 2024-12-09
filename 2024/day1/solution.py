ac4c28408b748f1a26f1a4e1b1a8f5d7d7cc44d4
Òscar del Álamo i Guaus <alamoscar24@gmail.com>
2024-12-09 11:41:36 +0100

    Day 2 solution

---
+++ 2024/day1/solution.py
@@ -0,0 +1,47 @@
+import numpy as np
+
+
+def part1(lists):
+    left_list, right_list = get_sorted_lists(lists)
+    distances = []
+    for left, right in zip(left_list, right_list):
+        distances.append(abs(left-right))
+    print(f"Solution part1: {sum(distances)}")
+
+
+def part2(lists):
+    left_list, right_list = get_sorted_lists(lists)
+    unique, counts = np.unique(right_list, return_counts=True)
+    similarity_score = []
+    for left in left_list:
+        count = np.count_nonzero(right_list == left)
+        if count != 0:
+            similarity_score.append(left * count)
+
+    print(f"Solution part2: {sum(similarity_score)}")
+
+
+def get_sorted_lists(lists):
+    left_list = []
+    right_list = []
+    for left, right in lists:
+        left_list.append(left)
+        right_list.append(right)
+    return np.sort(left_list), np.sort(right_list)
+
+
+def main():
+    with open("input.txt", "r") as file:
+        # Converts file into list
+        # strips newline
+        # splits each line by whitespace into an array
+        # casts each value of the array to integer
+        # in lists we have a list of arrays of lists of two integers each
+        lists = [list(map(int, line.strip().split())) for line in file]
+
+    part1(lists)
+    part2(lists)
+
+
+if __name__ == "__main__":
+    main()
---
+++ 2024/day2/solution.py
@@ -0,0 +1,64 @@
+def part1(reports):
+    safe_reports = []
+    for levels in reports:
+        # print(levels, report_is_safe(levels))
+        if report_is_safe(levels):
+            safe_reports.append(1)
+        else:
+            safe_reports.append(0)
+
+    print(f"Solution part1: {sum(safe_reports)}")
+
+
+def part2(reports):
+    safe_reports = []
+    for levels in reports:
+        # print(levels, report_is_safe(levels))
+        if report_is_safe(levels):
+            safe_reports.append(1)
+        else:
+            safe_w_removing = False
+            for index, _ in enumerate(levels):
+                levels_sliced = levels[:index] + levels[index + 1:]
+                if report_is_safe(levels_sliced):
+                    safe_w_removing = True
+                    break
+            if safe_w_removing:
+                safe_reports.append(1)
+            else:
+                safe_reports.append(0)
+
+    print(f"Solution part2: {sum(safe_reports)}")
+
+def report_is_safe(levels):
+    levels_asc = sorted(levels)
+    levels_desc = sorted(levels, reverse=True)
+    # Check levels order
+    if levels != levels_asc and levels != levels_desc:
+        # print(levels, "order", levels_asc, levels_desc)
+        return False
+    # Check increase/decrease restrictions
+    for index, value in enumerate(levels):
+        if len(levels) == index + 1:
+            break  # We have reached end
+        diff = abs(value - levels[index + 1])
+        if diff == 0 or diff > 3:
+            return False
+    return True  # In any other case
+
+
+def main():
+    with open("input.txt", "r") as file:
+        # Converts file into list
+        # strips newline
+        # splits each line by whitespace into an array
+        # casts each value of the array to integer
+        # in lists we have a list of arrays of lists of two integers each
+        reports = [list(map(int, line.strip().split())) for line in file]
+
+    part1(reports)
+    part2(reports)
+
+
+if __name__ == "__main__":
+    main()
